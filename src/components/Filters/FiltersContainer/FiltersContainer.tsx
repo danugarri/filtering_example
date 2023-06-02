beb69e861b7667dac773ae2f242675da86184483
danugarri <danugarri@hotmail.com>
2023-06-02 19:41:02 +0200

    style

--- src/components/Filters/FiltersContainer/FiltersContainer.tsx
+++ src/components/Filters/FiltersContainer/FiltersContainer.tsx
@@ -1,18 +1,20 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Select } from '../Select/Select';
 import { DataContext } from '../../DataProvider/DataProvider';
 import { getPossibleOptions } from '../../../helpers/helpers';
+import './FiltersContainer.css';
 
 export const FiltersContainer = () => {
   const data = useContext(DataContext);
 
   return (
-    <div>
+    <div className="filters-container">
+      <span className="filters-head">FILTERS</span>
       {data ? (
-        <>
+        <section className="selects-section">
           <Select options={getPossibleOptions(data, 'country')} />
           <Select options={getPossibleOptions(data, 'version')} />
-        </>
+        </section>
       ) : (
         <></>
       )}
